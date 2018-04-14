d6803dab0bd50df84b5dc9994f5aa0d9ece9c3c0
ali ozcan <aliozcan@gmail.com>
2018-04-14 14:19:41 +0200

    rental houses first commit

--- fundanl/main.py
+++ fundanl/main.py
@@ -6,6 +6,7 @@ import re
 import schedule
 import time
 import psycopg2
+from itertools import chain
 from collections import OrderedDict
 from requests_html import HTMLSession
 
@@ -25,16 +26,24 @@ def requests_html():
     session = HTMLSession()
     
     urls = ['https://www.funda.nl/koop/gemeente-rotterdam/', 
-            'https://www.funda.nl/koop/berkel-en-rodenrijs/']
+            'https://www.funda.nl/koop/berkel-en-rodenrijs/',
+            'https://www.funda.nl/huur/gemeente-rotterdam/']
     
     max_page_regex = r'p([0-9]+)/$'
     huis_link_regex = r'.+(huis|appartement)-\d+.+/'
-    
+    link_regex = r'(\w+)/(\w+)/(huis|appartement)-\d+.+' # koop/rotterdam/huis-40536936-petrus-trousselotstr
+
     try:
-        existing_houses = set(line.split(';')[0] for line in open('output/result.csv', 'r'))
-    except FileNotFoundError:
+        cur = conn.cursor()
+        cur.execute('select distinct url from fundanl')
+        existing_houses = cur.fetchall()
+        existing_houses = set(chain(*existing_houses))
+    except Exception as e:
+        logger.info(f'{e}')
         existing_houses = set()
-    
+    finally:
+        cur.close()
+ 
     for region_url in urls:
         r = session.get(region_url, headers=headers, timeout=5)
         max_page = [re.search(max_page_regex, link) for link in r.html.links]
@@ -42,21 +51,21 @@ def requests_html():
         logger.info(f'Max page: {max_page}')
         result = OrderedDict()
         try:
-            for page in range(1, 2):#max_page + 1):
+            for page in range(1, max_page + 1):
                 url = f'{region_url}p{page}/'           
                 r = session.get(url, headers=headers)
                 houses = [re.search(huis_link_regex, link) for link in r.html.links]
                 houses = [f'https://www.funda.nl{link.group(0)}' for link in houses if link]
                 house_count = len(houses)
                 logger.info(f'starting page {page}, url: {url}, # houses: {house_count}')
-                houses = [house for house in houses if re.search(r'(huis|appartement)-\d+.+', house).group(0).replace('/', '') not in existing_houses]
+                houses = [house for house in houses if re.search(huis_link_regex, house).group(0).replace('/', '') not in existing_houses]
                 logger.info(f'{house_count - len(houses)} links already scraped.')
                 
                 sleep_t = random.randint(2, 4)
                 time.sleep(sleep_t)
 
                 for count, house_url in enumerate(houses):
-                    house_url_postfix = re.search(r'(huis|appartement)-\d+.+', house_url).group(0).replace('/', '')
+                    house_url_postfix = re.search(link_regex, house_url).group(0)
                     existing_houses.add(house_url_postfix)
                     logger.info(f'page {page}, house {count + 1}, url: {house_url} parsing')
 
@@ -95,7 +104,7 @@ def write_results_to_db(result):
     try:
         cur = conn.cursor()
         for k, v in result.items():
-            cur.execute("insert into fundanl_sale(url, body) values (%s, %s)", (k, json.dumps(v)))
+            cur.execute("insert into fundanl(url, body) values (%s, %s)", (k, json.dumps(v)))
             conn.commit()
             logger.info(f'{len(result)} items are written.')
     except Exception as e:
@@ -109,7 +118,7 @@ def write_results_to_db(result):
 def init_db():
     try:
         cur = conn.cursor()
-        cur.execute('create table if not exists fundanl_sale(url text, body json);')
+        cur.execute('create table if not exists fundanl(url text, body json);')
         conn.commit()
     except:
         conn.rollback()
@@ -120,7 +129,7 @@ def init_db():
 def main():
     init_db()
     requests_html()
-    schedule.every().day.at('20:30').do(requests_html)
+    schedule.every().day.at('12:00').do(requests_html)
     while True:
         schedule.run_pending()
         time.sleep(1)
