21d9991668fb48d4540d925696c43e3170ffc5e9
Ali Ozcan <ozcan@pointlogic.com>
2018-05-30 09:52:56 +0200

    up an running geocoder

--- fundanl/geocoder/tasks.py
+++ fundanl/geocoder/tasks.py
@@ -11,34 +11,20 @@ logger = get_task_logger(__name__)
           task_time_limit=60, task_soft_time_limit=50)
 def geocode(self, url: str, address: str) -> bool:
     session = get_session(use_proxy=False, default_header=True)
-    url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    rurl = 'https://maps.googleapis.com/maps/api/geocode/json'
     params = {'sensor': 'false',
               'address': f'{address}',
               'region': 'nl'}
     session.params = params
     try:
         logger.info(f'geocoding {url}')
-        r = session.get(url, timeout=5)
-    except Exception as e:
-        num_retries = self.request.retries
-        seconds_to_wait = 2.0 ** num_retries
-        raise self.retry(countdown=seconds_to_wait)
-    else:
+        r = session.get(rurl, timeout=5)
         if r.status_code == 200:
-            try:
-                results = r.json()['results']
-                if not len(results) > 0:
-                    raise Exception('Result length is 0')
-                elif results['status'] == 'OVER_QUERY_LIMIT':
-                    raise Exception('Query Limit Exceed')
-                else:
-                    location = results[0]['geometry']['location']
-                    return write_geocode_to_db(url, location)
-            except KeyError as e:
-                logger.info(f'Wrong keys for dict. {r.text}')
-            except Exception as e:
-                # retry sometime tomorrow
-                raise self.retry(countdown=90000)
-        else:
-            logger.info(f'status code {r.status_code}. {url}')
-            raise ValueError('status code is not 200')
+            result = r.json()
+            if result['status'] == 'OK' and len(result['results']) > 0:
+                results = result['results']
+                location = results[0]['geometry']['location']
+                return write_geocode_to_db(url, location)
+    except Exception as e:
+        logger.info(f'{e}')
+        raise self.retry(countdown=90000)
